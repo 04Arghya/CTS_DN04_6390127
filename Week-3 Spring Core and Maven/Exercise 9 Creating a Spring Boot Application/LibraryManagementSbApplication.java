7a2ba6c03a38b3fb34de2deb1a7b4d97a60332a6
04Arghya <arghyadeepsingha045@gmail.com>
2025-07-06 00:26:02 +0530

    Added Week 3 assignments: Spring Data JPA and Spring Core with Maven

---
+++ Week-3 Spring Core and Maven/Exercise 9 Creating a Spring Boot Application/LibraryManagementSbApplication.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSbApplication.class, args);
+	}
+
+}
